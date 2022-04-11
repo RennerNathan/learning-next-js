1e2dd2f48ac2f285753d07a9225378b35f5cda35
Nathan Renner <nathanrenner68@gmail.com>
2022-04-11 13:09:00 -0500

    create ArticleItem for data fetching, ArticleList for showing data

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,32 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>WebDev News</title>
         <meta name='keywords' content='web development, programming'></meta>
       </Head>
-
-
-      <h1> Welcome to Next</h1>
+      {/* Pass in articles that were fetched and then returned as props */}
+      <ArticleList articles={articles} />
     </div>
   )
+}
+
+/* #3 Methods for Data Fetching:
+  Get Static Props: Fetch at build time,
+  Get server Side Props: Fetch at every request (SLOWER),
+  Get Static Paths: dynamically generate paths based on data you're fetching
+*/
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
